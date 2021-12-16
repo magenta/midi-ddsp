438a844a1953e94aab65928c91f3fa012e0bf3f9
lukewys <wuyusongwys@gmail.com>
2021-12-15 21:55:34 -0500

    upload model weights to model branch, learning from crepe

--- midi_ddsp/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Base module for the MIDI-DDSP library."""
-
-from midi_ddsp import data_handling
-from midi_ddsp import utils
-from midi_ddsp import modules
-from midi_ddsp import hparams_synthesis_generator
-from midi_ddsp.midi_ddsp_synthesize import synthesize_midi
